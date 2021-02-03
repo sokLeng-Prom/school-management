c69aa7a78f4e82406b5aba27281da5657c5d83bc
ashura_44056 <sengouy0@gmail.com>
2021-02-03 17:53:54 +0700

    fix bug in data table

--- src/components/DataTableDemo.js
+++ src/components/DataTableDemo.js
@@ -25,27 +25,13 @@ import FilterListIcon from "@material-ui/icons/FilterList";
 import { BASE_URL } from "../static/const";
 import axios from "axios";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function createData(courseCode, courseName, credit, faculty, description) {
   return { courseCode, courseName, credit, faculty, description };
 }
 
-var rows = [
-  // createData("Cupcake", 305, 3.7, 67, 4.3),
-  // createData("Donut", 452, 25.0, 51, 4.9),
-  // createData("Eclair", 262, 16.0, 24, 6.0),
-  // createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  // createData("Gingerbread", 356, 16.0, 49, 3.9),
-  // createData("Honeycomb", 408, 3.2, 87, 6.5),
-  // createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  // createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  // createData("KitKat", 518, 26.0, 65, 7.0),
-  // createData("Lollipop", 392, 0.2, 98, 0.0),
-  // createData("Marshmallow", 318, 0, 81, 2.0),
-  // createData("Nougat", 360, 19.0, 9, 37.0),
-  // createData("Oreo", 437, 18.0, 63, 4.0),
-];
+// var rows = [];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -124,13 +110,6 @@ function ExpandableTableRow({ children, expandComponent, ...otherProps }) {
 }
 
 function EnhancedTableHead(props) {
-  const fetch = async () => {
-    const resp = await axios.get(`${BASE_URL}/courses`);
-    rows = resp.data;
-    console.log(rows);
-  };
-  useEffect(() => fetch(), []);
-
   const {
     classes,
     // onSelectAllClick,
@@ -294,6 +273,14 @@ export default function DataTableDemo() {
   const [page, setPage] = React.useState(0);
   //   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = useState([]);
+  const fetch = async () => {
+    const resp = await axios.get(`${BASE_URL}/courses`);
+    setRows(resp.data);
+    // rows = resp.data;
+    // console.log(rows);
+  };
+  useEffect(() => fetch(), []);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
