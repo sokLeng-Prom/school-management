1ed88b537e4341921bd9e12fbea6f0e0d80228b9
ashura_44056 <sengouy0@gmail.com>
2021-02-05 11:15:18 +0700

    fix bug

--- src/components/DataTableDemo.js
+++ src/components/DataTableDemo.js
@@ -32,22 +32,6 @@ function createData(courseCode, courseName, credit, faculty, description) {
   return { courseCode, courseName, credit, faculty, description };
 }
 
-var rows = [
-  // createData("Cupcake", 305, 3.7, 67, 4.3),
-  // createData("Donut", 452, 25.0, 51, 4.9),
-  // createData("Eclair", 262, 16.0, 24, 6.0),
-  // createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  // createData("Gingerbread", 356, 16.0, 49, 3.9),
-  // createData("Honeycomb", 408, 3.2, 87, 6.5),
-  // createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  // createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  // createData("KitKat", 518, 26.0, 65, 7.0),
-  // createData("Lollipop", 392, 0.2, 98, 0.0),
-  // createData("Marshmallow", 318, 0, 81, 2.0),
-  // createData("Nougat", 360, 19.0, 9, 37.0),
-  // createData("Oreo", 437, 18.0, 63, 4.0),
-];
-
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -125,12 +109,12 @@ function ExpandableTableRow({ children, expandComponent, ...otherProps }) {
 }
 
 function EnhancedTableHead(props) {
-  const fetch = async () => {
-    const resp = await axios.get(`${BASE_URL}/courses`);
-    rows = resp.data;
-    console.log(rows);
-  };
-  useEffect(() => fetch(), []);
+  // const fetch = async () => {
+  //   const resp = await axios.get(`${BASE_URL}/courses`);
+  //   rows = resp.data;
+  //   console.log(rows);
+  // };
+  // useEffect(() => fetch(), []);
 
   const {
     classes,
@@ -194,13 +178,13 @@ const useToolbarStyles = makeStyles((theme) => ({
   highlight:
     theme.palette.type === "light"
       ? {
-        color: theme.palette.secondary.main,
-        backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-      }
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
       : {
-        color: theme.palette.text.primary,
-        backgroundColor: theme.palette.secondary.dark,
-      },
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
   title: {
     flex: "1 1 100%",
   },
@@ -287,7 +271,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function DataTableDemo() {
+export default function DataTableDemo(props) {
   const classes = useStyles();
   const [order, setOrder] = React.useState("asc");
   const [orderBy, setOrderBy] = React.useState("calories");
@@ -347,7 +331,7 @@ export default function DataTableDemo() {
   const isSelected = (name) => selected.indexOf(name) !== -1;
 
   const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    rowsPerPage - Math.min(rowsPerPage, props.rows.length - page * rowsPerPage);
 
   const [showMore, setShowmore] = useState(false);
   return (
@@ -368,10 +352,10 @@ export default function DataTableDemo() {
               orderBy={orderBy}
               //   onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
-              rowCount={rows.length}
+              rowCount={props.rows.length}
             />
             <TableBody>
-              {stableSort(rows, getComparator(order, orderBy))
+              {stableSort(props.rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
                   {
@@ -403,10 +387,20 @@ export default function DataTableDemo() {
                       <TableCell align="center">{row.credit}</TableCell>
                       <TableCell align="center">{row.faculty}</TableCell>
                       {console.log(showMore)}
-                      <TableCell align="center">{row.description.title}
-                      <span style={{ display: {showMore} ? "block" : "none" }}>{row.description.allText}</span>
-                      <span onClick={() => setShowmore(true)}>See More</span>
-                      <span style={{display: !{showMore} ? "block" : "none"}} onClick={() => setShowmore(false)}>See Less</span>
+                      <TableCell align="center">
+                        {row.description.title}
+                        <span
+                          style={{ display: { showMore } ? "block" : "none" }}
+                        >
+                          {row.description.allText}
+                        </span>
+                        <span onClick={() => setShowmore(true)}>See More</span>
+                        <span
+                          style={{ display: !{ showMore } ? "block" : "none" }}
+                          onClick={() => setShowmore(false)}
+                        >
+                          See Less
+                        </span>
                       </TableCell>
                     </ExpandableTableRow>
                     /* <TableRow
@@ -444,7 +438,7 @@ export default function DataTableDemo() {
         <TablePagination
           rowsPerPageOptions={[5, 10, 25]}
           component="div"
-          count={rows.length}
+          count={props.rows.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onChangePage={handleChangePage}
