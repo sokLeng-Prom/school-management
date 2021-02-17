9cf2b382733cac5e4a9c13b8c61d13be9961bffe
ashura_44056 <sengouy0@gmail.com>
2021-02-18 00:16:21 +0700

    assign course

--- src/components/FormDialogue.js
+++ src/components/FormDialogue.js
@@ -12,7 +12,7 @@ import CheckboxList from "./CheckboxList";
 import axios from "axios";
 import { BASE_URL } from "../static/const";
 
-export default function FormDialog() {
+export default function FormDialog(props) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -23,13 +23,100 @@ export default function FormDialog() {
     setOpen(false);
   };
 
-  const [courses, setCourses] = useState([]);
-  const fetchCourse = async () => {
-    const resp = await axios.get(`${BASE_URL}/courses`);
-    console.log(resp.data);
+  const [options, setOptions] = useState([]);
+
+  const [option, setOption] = useState("");
+  const optionHandler = (e) => {
+    setOption(e.target.value);
   };
 
-  useEffect(() => fetchCourse(), []);
+  // checklist
+  const [checkedCourse, setCheckedCourse] = React.useState([]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checkedCourse.indexOf(value);
+    const newChecked = [...checkedCourse];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setCheckedCourse(newChecked);
+  };
+
+  // onsubmit form
+  const onSubmitTeacher = async () => {
+    const username = option.split(" - ")[1];
+    const query = props.optionsData.filter(
+      (data) => data.username === username
+    )[0];
+    // query.id
+
+    const resp = await axios.get(`${BASE_URL}/users/${query.id}`);
+    var user = resp.data;
+    checkedCourse.map((course) => {
+      const prefix = course.split(" - ")[0];
+      user.data.class[prefix] = user.data.class[prefix] || {
+        students: [],
+        assignment: [],
+        quiz: [],
+      };
+    });
+    await axios.patch(`${BASE_URL}/users/${query.id}`, user);
+  };
+
+  const onSubmitStudent = async () => {
+    const username = option.split(" - ")[1];
+    const query = props.optionsData.filter(
+      (data) => data.username === username
+    )[0];
+    // query.id
+
+    const resp = await axios.get(`${BASE_URL}/users/${query.id}`);
+    var user = resp.data;
+    checkedCourse.map((course) => {
+      const prefix = course.split(" - ")[0];
+      user.data.class[prefix] = user.data.class[prefix] || {
+        courseCode: course.split(" - ")[0],
+        courseName: course.split(" - ")[1],
+        data: {
+          attendance: [],
+          assignment: [],
+          quiz: [],
+          notice: [],
+        },
+      };
+    });
+    await axios.patch(`${BASE_URL}/users/${query.id}`, user);
+  };
+
+  const onSubmit = () => {
+    if (props.role === "teacher") onSubmitTeacher();
+    if (props.role === "student") onSubmitStudent();
+  };
+
+  // const [coursesList, setCoursesList] = useState([]);
+  // const [courseOption, setCourseOption] = useState("");
+
+  // const courseOptionHandler = (e) => {
+  //   setCourseOption(e.target.value);
+  //   console.log(e.target.value);
+  // };
+
+  // const fetchCourse = async () => {
+  //   const resp = await axios.get(`${BASE_URL}/courses`);
+  //   console.log(resp.data);
+
+  //   const newArray = resp.data.map(
+  //     (map) => `${map.courseCode} - ${map.courseName}`
+  //   );
+
+  //   setCoursesList(newArray);
+  // };
+
+  useEffect(() => setOptions(props.options), []);
 
   return (
     <div>
@@ -55,16 +142,21 @@ export default function FormDialog() {
             fullWidth
           /> */}
           <CustomizedSelect
-            options={["A", "B", "C"]}
-            option={"Please Choose Something"}
+            options={props.options}
+            option={option}
+            optionHandler={optionHandler}
           />
-          <CheckboxList />
+          <CheckboxList
+            checked={checkedCourse}
+            handleToggle={handleToggle}
+            list={props.coursesList}
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={onSubmit} color="primary">
             Assign
           </Button>
         </DialogActions>
